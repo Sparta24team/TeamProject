ccad262fea010078f7fd1810664101786e8c2d4f
naminsu <naminsoo1020@naver.com>
2024-08-02 20:05:21 +0900

    [FEAT] Student 클래스 변경

--- src/camp/model/Student.java
+++ src/camp/model/Student.java
@@ -1,21 +1,22 @@
 package camp.model;
 
+import java.util.List;
+
 public class Student {
     private String studentId;
     private String studentName;
+    private List<Subject> subjects;
 
-    public Student(String seq, String studentName) {
+    public Student(String seq, String studentName, List<Subject> subjects) {
         this.studentId = seq;
         this.studentName = studentName;
+        this.subjects = subjects;
     }
 
     // Getter
-    public String getStudentId() {
-        return studentId;
-    }
+    public String getStudentId() { return studentId; }
 
-    public String getStudentName() {
-        return studentName;
-    }
+    public String getStudentName() { return studentName; }
+
 
 }
