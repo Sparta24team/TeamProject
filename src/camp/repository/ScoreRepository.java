6174da791490500fac347a564a342fd4ea1f6936
bronsta <bronsta111@gmail.com>
2024-08-06 16:01:18 +0900

    [REFACTOR] ScoreRepository 싱글톤 패턴 적용

--- src/camp/repository/ScoreRepository.java
+++ src/camp/repository/ScoreRepository.java
@@ -1,17 +1,29 @@
 package camp.repository;
 
 import camp.model.Score;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ScoreRepository {
 
     private static final String SCORE_CODE = "SC";
 
+    private static ScoreRepository instance;
+
     private final List<Score> scoreStore;
     private int sequence = 0;
 
-    public ScoreRepository(List<Score> scoreStore) {
-        this.scoreStore = scoreStore;
+    private ScoreRepository() {
+        this.scoreStore = new ArrayList<>();
+    }
+
+    public static ScoreRepository getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new ScoreRepository();
+            return instance;
+        }
+        return instance;
     }
 
     public void save(Score score) {
