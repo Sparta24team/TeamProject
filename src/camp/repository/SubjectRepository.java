4681beb01612c49e86e535c37734268a3eee4916
bronsta <bronsta111@gmail.com>
2024-08-06 15:12:39 +0900

    [REFACTOR] SubjectRepository 싱글톤 패턴 적용

--- src/camp/repository/SubjectRepository.java
+++ src/camp/repository/SubjectRepository.java
@@ -1,17 +1,31 @@
 package camp.repository;
 
 import camp.model.Subject;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class SubjectRepository {
 
     private static final String SUBJECT_CODE = "SU";
 
+    private static SubjectRepository instance;
+
     private final List<Subject> subjectStore;
     private int sequence = 0;
 
-    public SubjectRepository(List<Subject> subjectStore) {
-        this.subjectStore = subjectStore;
+    private SubjectRepository() {
+        this.subjectStore = new ArrayList<>();
+    }
+
+    public static SubjectRepository getInstance() {
+        if (Objects.isNull(instance)) {
+            instance = new SubjectRepository();
+            return instance;
+        }
+        return instance;
     }
 
     public void save(Subject subject) {
@@ -22,6 +36,12 @@ public class SubjectRepository {
 
     public List<Subject> findAll() {
         return subjectStore;
+    }
+
+    public List<Subject> findAllById(Set<String> subjectIds) {
+        return subjectStore.stream()
+                .filter(subject -> subjectIds.contains(subject.getSubjectId()))
+                .collect(Collectors.toList());
     }
 
     private String makeSubjectId() {
