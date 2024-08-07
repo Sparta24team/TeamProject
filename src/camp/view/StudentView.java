63b5db3639aa7dad87ceb7607abdddc239e235af
zerodeok <dudejr3165@naver.com>
2024-08-07 14:16:53 +0900

    [del] 중복 코드 수정

--- src/camp/view/StudentView.java
+++ src/camp/view/StudentView.java
@@ -21,12 +21,7 @@ public class StudentView {
     public void displayStudentView() {
         boolean flag = true;
         while (flag) {
-            System.out.println("==================================");
-            System.out.println("수강생 관리 실행 중...");
-            System.out.println("1. 수강생 등록");
-            System.out.println("2. 수강생 목록 조회");
-            System.out.println("3. 메인 화면 이동");
-            System.out.print("관리 항목을 선택하세요...");
+
             System.out.println("\n======================================================");
             System.out.println("||\t\t\t\t수강생 관리 실행 중...\t\t\t\t\t||");
             System.out.println("||\t\t\t\t\t\t\t\t\t\t\t\t\t||");
